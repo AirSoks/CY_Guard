55559e4e3de71371db26907c37367eb7370819f3
Matheo <Matheo@GAMING>
2025-02-02 04:22:34 +0100

    Config potable pour le spawn des tuiles

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -10,7 +10,7 @@ public class GameConfiguration {
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 800;
 	
-	public static final int BLOCK_SIZE = 40;
+	public static final int BLOCK_SIZE = 30;
 	
 	public static final int GAME_SPEED = 1000;
 
@@ -18,19 +18,19 @@ public class GameConfiguration {
 	public static final int VITESSE_GARDIENS = 12;
 	
 	public static final int DIFFICULTE = 1;
-	public static final int NB_CASE_DENSITE = 5;
+	public static final int NB_CASE_DENSITE = 1;
 	
-	public static final int NB_LIGNE = 10;
-	public static final int NB_COLONNE = 10;
+	public static final int NB_LIGNE = 27;
+	public static final int NB_COLONNE = 27;
 	
-	public static final int DENSITE_ROCHE = 20;
+	public static final int DENSITE_ROCHE = 150;
 	public static final int NB_ROCHE = (NB_LIGNE*NB_COLONNE)/(8*DIFFICULTE);
 	
 	public static final int DENSITE_ARBRE = 5;
-	public static final int NB_ARBRE = (NB_LIGNE*NB_COLONNE)/(10*DIFFICULTE);	
+	public static final int NB_ARBRE = (NB_LIGNE*NB_COLONNE)/(5*DIFFICULTE);	
 	
-	public static final int DENSITE_LAC = 40;
-	public static final int NB_LAC = (NB_LIGNE*NB_COLONNE)/(6*DIFFICULTE);	
+	public static final int DENSITE_LAC = 500;
+	public static final int NB_LAC = (NB_LIGNE*NB_COLONNE)/(8*DIFFICULTE);	
 	
 	public static final Plaine PLAINE = new Plaine();
 	public static final Arbre ARBRE = new Arbre();

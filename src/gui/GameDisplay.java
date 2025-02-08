f692634887d4b36b669b6bff4b57356be6007c65
existenzin <rigsangzurkhang@gmail.com>
2025-02-08 15:13:28 +0100

    ajout de gardien dans gamedisplay

--- src/gui/GameDisplay.java
+++ src/gui/GameDisplay.java
@@ -1,24 +1,28 @@
-package gui;
-
-import java.awt.Graphics;
-
-import javax.swing.JPanel;
-
-import engine.map.Grille;
-
-public class GameDisplay extends JPanel{
+	package gui;
 	
-	private Grille grille;
+	import java.awt.Graphics;
 	
-	private PaintStrategy paintStrategy = new PaintStrategy();
+	import javax.swing.JPanel;
 	
-	public GameDisplay(Grille grille) {
-		this.grille = grille;
-	}
-
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+	import engine.map.Grille;
+	import engine.personnage.Gardien;
+	
+	public class GameDisplay extends JPanel{
 		
-		paintStrategy.paint(grille, g);
+		private Grille grille;
+		private Gardien gardien;
+		
+		private PaintStrategy paintStrategy = new PaintStrategy();
+		
+		public GameDisplay(Grille grille, Gardien gardien) {
+			this.grille = grille;
+			this.gardien = gardien;
+		}
+	
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			paintStrategy.paint(grille, g);
+			paintStrategy.paint(gardien, g);
+		}
 	}
-}

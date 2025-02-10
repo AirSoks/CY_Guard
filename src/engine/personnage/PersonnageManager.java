67aaa4fca28c39ec54e61cc6edc8af826cf13149
mathe <mathe@DESKTOP-3O5QJKT>
2025-02-10 22:53:05 +0100

    Correction des déplacement des personnages

--- src/engine/personnage/PersonnageManager.java
+++ src/engine/personnage/PersonnageManager.java
@@ -1,52 +1,57 @@
 package engine.personnage;
 
 import config.GameConfiguration;
+import engine.map.Coordonnee;
 import engine.map.Case;
 import engine.map.Grille;
 
 public class PersonnageManager {
-		private Grille grille;
+	private Grille grille;
 		
-		private Gardien gardien;
+	private Gardien gardien;
 		
-		public PersonnageManager(Grille grille, Gardien gardien) {
-			this.grille = grille;
-			this.gardien = gardien;
-		}
+	public PersonnageManager(Grille grille, Gardien gardien) {
+		this.grille = grille;
+		this.gardien = gardien;
+	}
 		
-		public void moveLeftGardien() {
-			Case position = gardien.getCase();
-			
-			if(position.getPosition().getColonne() > 0) {
-				Case newposition = grille.getCase(position.getPosition().getLigne()-1, position.getPosition().getColonne());
-				gardien.setCase(newposition);
+	public void moveLeftGardien() {
+		Coordonnee position = gardien.getCoordonnee();
+		if (position.getColonne() > 0) {
+			Case newCase = grille.getCase(position.getLigne(), position.getColonne() - 1);
+			if (newCase != null) {
+				gardien.setCoordonnee(newCase.getPosition());
 			}
 		}
+	}
 		
-		public void moveRightGardien() {
-			Case position = gardien.getCase();
-			
-			if(position.getPosition().getColonne() < GameConfiguration.NB_COLONNE - 1) {
-				Case newposition = grille.getCase(position.getPosition().getLigne()+ 1, position.getPosition().getColonne() );
-				gardien.setCase(newposition);
+	public void moveRightGardien() {
+		Coordonnee position = gardien.getCoordonnee();
+		if (position.getColonne() < GameConfiguration.NB_COLONNE - 1) {
+			Case newCase = grille.getCase(position.getLigne(), position.getColonne() + 1);
+			if (newCase != null) {
+				gardien.setCoordonnee(newCase.getPosition());
 			}
 		}
+	}
 		
-		public void moveUpGardien() {
-			Case position = gardien.getCase();
-			
-			if(position.getPosition().getLigne() > 0) {
-				Case newposition = grille.getCase(position.getPosition().getLigne() , position.getPosition().getColonne() - 1);
-				gardien.setCase(newposition);
+	public void moveUpGardien() {
+		Coordonnee position = gardien.getCoordonnee();
+		if (position.getLigne() > 0) {
+			Case newCase = grille.getCase(position.getLigne() - 1, position.getColonne());
+			if (newCase != null) {
+				gardien.setCoordonnee(newCase.getPosition());
 			}
 		}
-		
-		public void moveDownGardien() {
-			Case position = gardien.getCase();
-			
-			if(position.getPosition().getColonne() < GameConfiguration.NB_LIGNE - 1) {
-				Case newposition = grille.getCase(position.getPosition().getLigne() , position.getPosition().getColonne()+ 1);
-				gardien.setCase(newposition);
+	}
+	
+	public void moveDownGardien() {
+		Coordonnee position = gardien.getCoordonnee();
+		if (position.getLigne() < GameConfiguration.NB_LIGNE - 1) {
+			Case newCase = grille.getCase(position.getLigne() + 1, position.getColonne());
+			if (newCase != null) {
+				gardien.setCoordonnee(newCase.getPosition());
 			}
 		}
+	}
 }

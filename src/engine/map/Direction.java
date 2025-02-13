bef32331e38b147149fd35346d6b8affa474908a
mathe <mathe@DESKTOP-3O5QJKT>
2025-02-13 01:06:37 +0100

    Création d'une class de Direction by @Menaouer

---
+++ src/engine/map/Direction.java
@@ -0,0 +1,21 @@
+package engine.map;
+
+public enum Direction {
+	HAUT(-1,0),
+	BAS(1,0),
+	GAUCHE(0,-1),
+	DROITE(0,1);
+	
+	private final int deltaLigne, deltaColonne;
+	
+	Direction(int deltaLigne, int deltaColonne){
+		this.deltaLigne = deltaLigne;
+		this.deltaColonne = deltaColonne;
+	}
+	
+	public Coordonnee getCoordonnee(Coordonnee coordonnee) {
+		int nouvelleLigne = coordonnee.getLigne() + this.deltaLigne;
+        int nouvelleColonne = coordonnee.getColonne() + this.deltaColonne;
+		return new Coordonnee(nouvelleLigne, nouvelleColonne);
+	}
+}

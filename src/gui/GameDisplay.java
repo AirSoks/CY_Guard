d7342a17718951b45b4f146e934e943019de206c
mathe <mathe@DESKTOP-3O5QJKT>
2025-02-13 01:01:37 +0100

    Indentation à gauche

--- src/gui/GameDisplay.java
+++ src/gui/GameDisplay.java
@@ -1,28 +1,27 @@
 	package gui;
 	
-	import java.awt.Graphics;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+import engine.map.Grille;
+import engine.personnage.Gardien;
 	
-	import javax.swing.JPanel;
-	
-	import engine.map.Grille;
-	import engine.personnage.Gardien;
-	
-	public class GameDisplay extends JPanel{
+public class GameDisplay extends JPanel{
 		
-		private Grille grille;
-		private Gardien gardien;
+	private Grille grille;
+	private Gardien gardien;
 		
-		private PaintStrategy paintStrategy = new PaintStrategy();
+	private PaintStrategy paintStrategy = new PaintStrategy();
 		
-		public GameDisplay(Grille grille, Gardien gardien) {
-			this.grille = grille;
-			this.gardien = gardien;
-		}
-	
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			
-			paintStrategy.paint(grille, g);
-			paintStrategy.paint(gardien, g);
-		}
+	public GameDisplay(Grille grille, Gardien gardien) {
+		this.grille = grille;
+		this.gardien = gardien;
 	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+			
+		paintStrategy.paint(grille, g);
+		paintStrategy.paint(gardien, g);
+	}
+}

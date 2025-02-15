ed8c43930044f89d174327917b40f49f7a629cf3
mathe <mathe@DESKTOP-3O5QJKT>
2025-02-15 02:19:58 +0100

    Verification du contact entre gardien et intrus lord du deplacement

--- src/engine/personnage/gestion/PersonnageDeplacement.java
+++ src/engine/personnage/gestion/PersonnageDeplacement.java
@@ -1,14 +1,17 @@
 package engine.personnage.gestion;
 
+import java.util.List;
+
 import engine.map.Case;
 import engine.map.Coordonnee;
 import engine.map.Direction;
 import engine.map.Grille;
+import engine.personnage.Gardien;
 import engine.personnage.Personnage;
 
 public class PersonnageDeplacement {
 	
-	public boolean deplacerPersonnage(Grille grille, Personnage personnage, Direction direction) {
+	public static boolean deplacerPersonnage(Grille grille, Personnage personnage, Direction direction) {
 		Coordonnee nouvellePosition = direction.getCoordonnee(personnage.getCoordonnee());
 		if (isCoordonneeValide(grille, nouvellePosition)) {
 			personnage.setCoordonnee(nouvellePosition);
@@ -17,11 +20,29 @@ public class PersonnageDeplacement {
 		return false;
 	}
 	
-	private Boolean isCoordonneeValide(Grille grille, Coordonnee coordonnee) {
+	private static Boolean isCoordonneeValide(Grille grille, Coordonnee coordonnee) {
 		Case c = grille.getCase(coordonnee);
 		if (c != null && !c.getObstacle().isBloqueDeplacement()) {
 			return true;
 		}
 		return false;
+	}
+
+	public static void contactPersonnage(PersonnageManager personnageManager, Coordonnee coordonnee) { //
+		List<Personnage> personnages = personnageManager.getPersonnages(coordonnee);
+		int nbPersonnageInitial = personnages.size();
+
+    	if (nbPersonnageInitial >= 2) {
+    		for (Personnage personnage : personnages) {
+    			if (personnage instanceof Gardien) {
+    				personnages.remove(personnage);
+    			}
+    		}
+    		if (nbPersonnageInitial != personnages.size()) {
+    			for (Personnage personnage : personnages) {
+    				personnageManager.retirerPersonnage(personnage);
+    			}
+    		}
+    	}
 	}
 }

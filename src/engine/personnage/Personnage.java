392174616f52adcf2e03c37e6d0626a46607816a
existenzin <rigsangzurkhang@gmail.com>
2025-03-02 22:59:13 +0100

    ajout de class getsprite() pour les images et movement()

--- src/engine/personnage/Personnage.java
+++ src/engine/personnage/Personnage.java
@@ -1,8 +1,11 @@
 package engine.personnage;
 
+import java.awt.Image;
+
 import engine.map.Coordonnee;
 import engine.personnage.deplacement.Deplacement;
 import engine.utilitaire.GenerateurNom;
+import engine.utilitaire.SimulationUtility;
 
 /**
  * Personnage est une classe abstraite représentant un personnage dans la grille
@@ -40,11 +43,35 @@ public abstract class Personnage {
      * Le déplacement du personnage
      */
     private Deplacement deplacement;
+    
+    private int animationFrame = 1;
+    
+    private String dernierDirection = "BAS";
 	
 	public Personnage(Coordonnee coordonnee) {
 		this.coordonnee = coordonnee;
 		this.name = GenerateurNom.genererNom();
 		this.tempsInvocation = System.currentTimeMillis();
+	}
+	
+	public Image getSprite() {
+	    String type = "intrus";
+
+	    if (this instanceof Gardien) {
+	        type = "guard";
+	    }
+
+	    String fileName = "./images/" + type + dernierDirection.toLowerCase() + animationFrame + ".png";
+	    return SimulationUtility.readImage(fileName);
+	}
+	
+	public void movement(String direction) {
+		this.dernierDirection = direction;
+		if (animationFrame == 1) {
+	        animationFrame = 2;
+	    } else {
+	        animationFrame = 1;
+	    }
 	}
 	
 	public Coordonnee getCoordonnee() {
